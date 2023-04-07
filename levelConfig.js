aa377bd37ba6cf068e0738a93c5c61b6df513d66
Jeremy Kozakiewicz <jeremy@Jakubs-iMac.local>
2023-04-07 13:58:42 +1000

    level config

---
+++ levelConfig.js
@@ -0,0 +1,30 @@
+const levels = {
+    level0: {
+        name: 'Level 0',
+        data: [],
+        next: 'level1',
+        startX: 100,
+        startY: 100,
+    },
+    level1: {
+        name: 'Level 1',
+        data: mapLevel1,
+        next: 'level2',
+        startX: 100,
+        startY: 100,
+    },
+    level2: {
+        name: 'Level 2',
+        data: mapLevel2,
+        next: 'level3',
+        startX: 50,
+        startY: 200,
+    },
+    level3: {
+        name: 'Level 3',
+        data: mapLevel3,
+        next: 'level1',
+        startX: 50,
+        startY: 200,
+    },
+}
--- play.js
+++ play.js
@@ -7,37 +7,6 @@ let elvio,
     scale = 2,
     currentLevel = 'level0'
 
-const levels = {
-    level0: {
-        name: 'Level 0',
-        data: [],
-        next: 'level1',
-        startX: 100,
-        startY: 100,
-    },
-    level1: {
-        name: 'Level 1',
-        data: mapLevel1,
-        next: 'level2',
-        startX: 100,
-        startY: 100,
-    },
-    level2: {
-        name: 'Level 2',
-        data: mapLevel2,
-        next: 'level3',
-        startX: 50,
-        startY: 200,
-    },
-    level3: {
-        name: 'Level 3',
-        data: mapLevel3,
-        next: 'level1',
-        startX: 50,
-        startY: 200,
-    },
-}
-
 const drawLevel = (data) => {
     let tileNo = 0
     let exitNo = 0
